11a8f64bccf5e5c3350c30cb62f894383df6d69b
Gurnoor singh <gurnoorjaura44@gmail.com>
2025-08-15 20:55:54 +0530

    g

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ import formData from "./middlewares/formData.js";
 import fileData from "./middlewares/fileData.js";
 import bucket from "./firebase.js";
 import fetch from "node-fetch";
- import path from "path";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
