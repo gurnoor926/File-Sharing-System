dc5f95f687f1ed8283efe0ecb37ee457ce94601c
Gurnoor singh <gurnoorjaura44@gmail.com>
2025-08-15 22:40:57 +0530

    j

--- Backend/index.js
+++ Backend/index.js
@@ -198,7 +198,7 @@ app.delete("/delete_file/:id",authenticateToken, async (req,res)=>{
     console.error(error);
   }
 });
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'../Frontend/my-app/dist','index.html'));
+app.get('/*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'../Frontend/my-app/dist','index.html'))
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
