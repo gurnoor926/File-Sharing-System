12ae08d86ca1e67607b638b592f9321bd1236a61
Gurnoor singh <gurnoorjaura44@gmail.com>
2025-08-15 20:24:19 +0530

    upload files

--- Backend/firebase.js
+++ Backend/firebase.js
@@ -1,11 +1,11 @@
 import admin from 'firebase-admin';
-import {readFileSync} from 'fs';
-const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT
-);
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "sendora-download.firebasestorage.app"
+    credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 })
 const bucket = admin.storage().bucket();
 export default bucket;
